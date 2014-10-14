e53ce4fe1da66c86c0eb9105b412e3951bf652e7
Tobbe <contact@ikascripts.de>
2014-10-15 00:46:33 +0200

    Version 1.2 for history purpose

--- release/ShowIncomeOnTop.user.js
+++ release/ShowIncomeOnTop.user.js
@@ -1,51 +1,65 @@
 // ==UserScript==
 // @name			Show Income On Top
-// @version			1.01
-// @namespace		http://*.ikariam.*/*
 // @description		Shows the actual income also on top of the site.
+// @namespace		http://*.ikariam.*/*
 // @author			TOBBE
+// @version			1.02
+//
 // @require			http://userscripts.org/scripts/source/57756.user.js
+//
 // @include			http://*.ikariam.*/index.php?view=finances
+//
 // @exclude			http://board.ikariam.*/*
 // @exclude			http://support.ikariam.*/*
 // @exclude			http://support.*.ikariam.*/*
-// @history			1.01	Update check added.
-// @history			1.00	First Version.
+//
+// @history			1.02	Cleaned up code
+// @history			1.02	Added Income in 24h
+// @history			1.01	Added Update check
+// @history			1.00	Initial release
 // ==/UserScript==
 
 // Update check
-ScriptUpdater.forceNotice(74221, 1.01);
+ScriptUpdater.check(74221, 1.02);
 
-// Script
-var hidden = document.getElementsByClassName('hidden');
+// Start script
+main();
 
-var income = hidden[hidden.length - 1].innerHTML;
+// Main-function of the script.
+function main() {
+	var income = getIncome();
+	
+	var balance = document.getElementById('balance');
+	
+	incomeRow = balance.insertRow(1);
+	incomeRow24h = balance.insertRow(2);
+	
+	createTableRow(new Array("+/-", "", "", formatToIkaNumber(income)), new Array("sigma", "value res", "value res", "value res"), incomeRow);
+	createTableRow(new Array("+/- in 24h", "", "", formatToIkaNumber(income * 24)), new Array("sigma", "value res", "value res", "value res"), incomeRow24h);
+}
 
-var balance = document.getElementById('balance');
+// Returns the actual income.
+function getIncome() {
+	var hidden = document.getElementsByClassName('hidden');
+	
+	return Number(hidden[hidden.length - 1].innerHTML.replace(".", ""));
+}
 
-incomeRow = balance.insertRow(1);
+// Formats a number to that format that is used in Ikariam.
+function formatToIkaNumber(num) {
+  num += "";
+  num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1.");
+  return num;
+}
 
-var cell1 = document.createElement("td");
-var cell2 = document.createElement("td");
-var cell3 = document.createElement("td");
-var cell4 = document.createElement("td");
-
-var cell1Text = document.createTextNode("+ / -");
-var cell2Text = document.createTextNode("");
-var cell3Text = document.createTextNode("");
-var cell4Text = document.createTextNode(income);
-
-cell1.appendChild(cell1Text);
-cell1.appendChild(cell2Text);
-cell1.appendChild(cell3Text);
-cell4.appendChild(cell4Text);
-
-cell1.className = "sigma";
-cell2.className = "value res";
-cell3.className = "value res";
-cell4.className = "hidden";
-
-incomeRow.appendChild(cell1);
-incomeRow.appendChild(cell2);
-incomeRow.appendChild(cell3);
-incomeRow.appendChild(cell4);
+// Adds cells to an table row.
+function createTableRow(cellText, cellClassName, row) {
+	for(var i = 0; i < 4; i++) {
+		var cell = document.createElement("td");
+		var cell_Text = document.createTextNode(cellText[i]);
+		
+		cell.appendChild(cell_Text);
+		cell.className = cellClassName[i];
+		row.appendChild(cell);
+	}
+}
