ff63f38a772a452566324d7c72d91882bf0242fc
Tobbe <contact@ikascripts.de>
2014-10-15 00:47:17 +0200

    Version 1.2.1 for history purpose

--- release/ShowIncomeOnTop.user.js
+++ release/ShowIncomeOnTop.user.js
@@ -3,7 +3,7 @@
 // @description		Shows the actual income also on top of the site.
 // @namespace		http://*.ikariam.*/*
 // @author			TOBBE
-// @version			1.02
+// @version			1.03
 //
 // @require			http://userscripts.org/scripts/source/57756.user.js
 //
@@ -13,14 +13,26 @@
 // @exclude			http://support.ikariam.*/*
 // @exclude			http://support.*.ikariam.*/*
 //
-// @history			1.02	Cleaned up code
-// @history			1.02	Added Income in 24h
-// @history			1.01	Added Update check
+// @history			1.03	Fixed: Bug with , as seperator
+// @history			1.03	Added: New style of update-panel
+// @history			1.02	Fixed: Cleaned up code
+// @history			1.02	Added: Income in 24h
+// @history			1.01	Added: Update check
 // @history			1.00	Initial release
 // ==/UserScript==
 
-// Update check
-ScriptUpdater.check(74221, 1.02);
+// Version and Script-ID
+var version = 1.03;
+var scriptId = 74221;
+
+// Seperator ist an point or not
+var point = true;
+
+// Set the Style
+setStyles();
+
+// Check for Updates
+ScriptUpdater.check(scriptId, version);
 
 // Start script
 main();
@@ -41,15 +53,29 @@ function main() {
 // Returns the actual income.
 function getIncome() {
 	var hidden = document.getElementsByClassName('hidden');
-	
-	return Number(hidden[hidden.length - 1].innerHTML.replace(".", ""));
+	var txt = hidden[hidden.length - 1].innerHTML;
+
+	if(txt.search(",") != -1) {
+		txt = txt.replace(",", "");
+		point = false;
+	} else {
+		txt = txt.replace(".", "");
+	}
+
+	return Number(txt);
 }
 
 // Formats a number to that format that is used in Ikariam.
 function formatToIkaNumber(num) {
-  num += "";
-  num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1.");
-  return num;
+	num += "";
+
+	if(point) {
+		num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1.");
+	} else {
+		num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1,");
+	}
+
+	return num;
 }
 
 // Adds cells to an table row.
@@ -62,4 +88,16 @@ function createTableRow(cellText, cellClassName, row) {
 		cell.className = cellClassName[i];
 		row.appendChild(cell);
 	}
+}
+
+function setStyles() {
+	// #ddb577 Dark
+	// #fdf7dd Bright
+	// #542c0f Font/Border
+	
+	GM_addStyle(
+			"#ScriptUpdater74221Mask { position:fixed !important; } \
+			#ScriptUpdater74221Body { position:fixed !important; background-color:#fdf7dd !important; border:#542c0f !important; color:#542c0f !important; } \
+			#ScriptUpdater74221Body h1 { background-color:#ddb577 !important; -moz-border-radius:5px !important;}"
+	);
 }
