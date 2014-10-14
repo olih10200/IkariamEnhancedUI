960240c3c8e8a38911af818b285e2ecb70f57668
Tobbe <contact@ikascripts.de>
2014-10-15 00:49:37 +0200

    Version 1.3.2 for history purpose

--- release/ShowIncomeOnTop.user.js
+++ release/ShowIncomeOnTop.user.js
@@ -3,9 +3,7 @@
 // @description		Shows the actual income also on top of the site.
 // @namespace		http://*.ikariam.*/*
 // @author			TOBBE
-// @version			1.05
-//
-// @require			http://www.betawarriors.com/bin/gm/57756user.js
+// @version			1.06
 //
 // @include			http://*.ikariam.*/index.php?view=finances
 //
@@ -13,8 +11,10 @@
 // @exclude			http://support.ikariam.*/*
 // @exclude			http://support.*.ikariam.*/*
 // 
-// @history			1.05	Added new Script Updater - will be replaced by my own Updater in the next few days.
-// @history			1.04	Fixed: Due to the Problem with Greasemonkey-Scripts I remove the Script-Updater. It has been written by the same guy who probably has written the infected Script.
+// @history			1.06	Added: Own Script-Updater.
+// @history			1.06	Fixed: Remove everything what refered to other Scripts.
+// @history			1.05	Added: New Script-Updater - will be replaced by my own Updater in the next few days.
+// @history			1.04	Fixed: Remove the Script-Updater (Because of the Problem with Greasemonkey-Scripts).
 // @history			1.03	Fixed: Bug with , as seperator
 // @history			1.03	Added: New style of update-panel
 // @history			1.02	Fixed: Cleaned up code
@@ -24,17 +24,17 @@
 // ==/UserScript==
 
 // Version and Script-ID
-var version = 1.05;
+var version = 1.06;
 var scriptId = 74221;
+
+// Check for Updates
+checkForUpdates();
 
 // Seperator ist an point or not
 var point = true;
 
 // Set the Style
 setStyles();
-
-// Check for Updates
-ScriptUpdater.check(scriptId, version);
 
 // Start script
 main();
@@ -48,8 +48,8 @@ function main() {
 	incomeRow = balance.insertRow(1);
 	incomeRow24h = balance.insertRow(2);
 	
-	createTableRow(new Array("+/-", "", "", formatToIkaNumber(income)), new Array("sigma", "value res", "value res", "value res"), incomeRow);
-	createTableRow(new Array("+/- in 24h", "", "", formatToIkaNumber(income * 24)), new Array("sigma", "value res", "value res", "value res"), incomeRow24h);
+	createTableRow(new Array('+/-', '', '', formatToIkaNumber(income)), new Array('sigma', 'value res', 'value res', 'value res'), incomeRow);
+	createTableRow(new Array('+/- in 24h', '', '', formatToIkaNumber(income * 24)), new Array('sigma', 'value res', 'value res', 'value res'), incomeRow24h);
 }
 
 // Returns the actual income.
@@ -57,11 +57,11 @@ function getIncome() {
 	var hidden = document.getElementsByClassName('hidden');
 	var txt = hidden[hidden.length - 1].innerHTML;
 
-	if(txt.search(",") != -1) {
-		txt = txt.replace(",", "");
+	if(txt.search(',') != -1) {
+		txt = txt.replace(',', '');
 		point = false;
 	} else {
-		txt = txt.replace(".", "");
+		txt = txt.replace('.', '');
 	}
 
 	return Number(txt);
@@ -69,12 +69,12 @@ function getIncome() {
 
 // Formats a number to that format that is used in Ikariam.
 function formatToIkaNumber(num) {
-	num += "";
+	num += '';
 
 	if(point) {
-		num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1.");
+		num = num.replace(/(\d)(?=(\d{3})+\b)/g,'$1.');
 	} else {
-		num = num.replace(/(\d)(?=(\d{3})+\b)/g,"$1,");
+		num = num.replace(/(\d)(?=(\d{3})+\b)/g,'$1,');
 	}
 
 	return num;
@@ -82,24 +82,75 @@ function formatToIkaNumber(num) {
 
 // Adds cells to an table row.
 function createTableRow(cellText, cellClassName, row) {
-	for(var i = 0; i < 4; i++) {
-		var cell = document.createElement("td");
-		var cell_Text = document.createTextNode(cellText[i]);
+	for(var i = 0; i < cellText.length; i++) {
+		var cell = document.createElement('td');
 		
-		cell.appendChild(cell_Text);
+		cell.innerHTML = cellText[i];
 		cell.className = cellClassName[i];
 		row.appendChild(cell);
 	}
 }
 
+// Checks for Updates of the Script.
+function checkForUpdates(){
+	GM_xmlhttpRequest ({
+		method: 'GET',
+		url: 'http://userscripts.org/scripts/source/74221.meta.js',
+		headers: {'User-agent': 'Mozilla/5.0', 'Accept': 'text/html'},
+		onload: function (response) {
+			var metadata = formatMetadata(response.responseText);
+
+			if(version < metadata.version) {
+				showUpdateInfo(metadata);
+			}
+		}
+	});
+}
+
+// Formats the given Metadata.
+function formatMetadata(metadataIn) {
+	var metadataOut = new Array();
+	var innerMeta = metadataIn.match(/\/\/ ==UserScript==((.|\n|\r)*?)\/\/ ==\/UserScript==/)[0];
+	
+	if(innerMeta) {
+		var tags = innerMeta.match(/\/\/ @(.*?)(\n|\r)/g);
+		
+		for(var i = 0; i < tags.length; i++) {
+			var tmp = tags[i].match(/\/\/ @(.*?)\s+(.*)/);
+			
+			if(!metadataOut[tmp[1]]) {
+				metadataOut[tmp[1]] = new Array (tmp[2]);
+				
+			} else {
+				metadataOut[tmp[1]].push(tmp[2]);
+				
+			}
+		}
+	}
+
+	return metadataOut;
+}
+
+// Shows the Update information
+function showUpdateInfo(metadata) {
+	balance = document.getElementById('balance');
+	
+	updateRow = balance.insertRow(3);
+	infoRow = balance.insertRow(4);
+	
+	var updateInfo = '<br><br>';
+	
+	for(var i = 0; i < metadata['history'].length; i++) {
+		updateInfo += metadata['history'][i] + '<br><br>';
+	}
+	
+	createTableRow(new Array('<u><a href="http://userscripts.org/scripts/show/74221">There is version ' + metadata.version + ' of Show Income On Top available.</a></u> You currently have version ' + version + ' installed.'), new Array('sigma'), updateRow);
+	createTableRow(new Array(updateInfo), new Array('sigma'), infoRow);
+}
+
+// Sets the Styles that are used.
 function setStyles() {
 	// #ddb577 Dark
 	// #fdf7dd Bright
 	// #542c0f Font/Border
-	
-	GM_addStyle(
-			"#ScriptUpdater74221Mask { position:fixed !important; } \
-			#ScriptUpdater74221Body { position:fixed !important; background-color:#fdf7dd !important; border:#542c0f !important; color:#542c0f !important; } \
-			#ScriptUpdater74221Body h1 { background-color:#ddb577 !important; -moz-border-radius:5px !important;}"
-	);
 }
