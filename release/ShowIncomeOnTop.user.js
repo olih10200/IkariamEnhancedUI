b38d92f9c877eba857339208c447fe8002b74d39
Tobbe <contact@ikascripts.de>
2014-10-15 00:48:53 +0200

    Version 1.3.1 for history purpose

--- release/ShowIncomeOnTop.user.js
+++ release/ShowIncomeOnTop.user.js
@@ -3,15 +3,18 @@
 // @description		Shows the actual income also on top of the site.
 // @namespace		http://*.ikariam.*/*
 // @author			TOBBE
-// @version			1.04
+// @version			1.05
+//
+// @require			http://www.betawarriors.com/bin/gm/57756user.js
 //
 // @include			http://*.ikariam.*/index.php?view=finances
 //
 // @exclude			http://board.ikariam.*/*
 // @exclude			http://support.ikariam.*/*
 // @exclude			http://support.*.ikariam.*/*
-//
-// @history			1.04	Fixed: Because of the Problem with Greasemonkey-Scripts I remove the Script-Updater. It has been written by the same guy who has written the infected Script.
+// 
+// @history			1.05	Added new Script Updater - will be replaced by my own Updater in the next few days.
+// @history			1.04	Fixed: Due to the Problem with Greasemonkey-Scripts I remove the Script-Updater. It has been written by the same guy who probably has written the infected Script.
 // @history			1.03	Fixed: Bug with , as seperator
 // @history			1.03	Added: New style of update-panel
 // @history			1.02	Fixed: Cleaned up code
@@ -20,8 +23,18 @@
 // @history			1.00	Initial release
 // ==/UserScript==
 
+// Version and Script-ID
+var version = 1.05;
+var scriptId = 74221;
+
 // Seperator ist an point or not
 var point = true;
+
+// Set the Style
+setStyles();
+
+// Check for Updates
+ScriptUpdater.check(scriptId, version);
 
 // Start script
 main();
@@ -77,4 +90,16 @@ function createTableRow(cellText, cellClassName, row) {
 		cell.className = cellClassName[i];
 		row.appendChild(cell);
 	}
+}
+
+function setStyles() {
+	// #ddb577 Dark
+	// #fdf7dd Bright
+	// #542c0f Font/Border
+	
+	GM_addStyle(
+			"#ScriptUpdater74221Mask { position:fixed !important; } \
+			#ScriptUpdater74221Body { position:fixed !important; background-color:#fdf7dd !important; border:#542c0f !important; color:#542c0f !important; } \
+			#ScriptUpdater74221Body h1 { background-color:#ddb577 !important; -moz-border-radius:5px !important;}"
+	);
 }
